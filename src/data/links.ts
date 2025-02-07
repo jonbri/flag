45918c33871db9c57a4d84445f24320140826479
Jonathan Brink <jonathandavidbrink@gmail.com>
2025-02-07 09:38:32 -0500

    team links

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,5 +1,13 @@
 export const links = [
   {
+    name: "Bills",
+    url: "https://en.wikipedia.org/wiki/Buffalo_Bills",
+  },
+  {
+    name: "Dolphins",
+    url: "https://en.wikipedia.org/wiki/Miami_Dolphins",
+  },
+  {
     name: "Chargers",
     url: "https://en.wikipedia.org/wiki/Los_Angeles_Chargers",
   },

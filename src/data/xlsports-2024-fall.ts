29d2c809d4d9ac5dbdcb04b45e1b649e78e02c0e
Jonathan Brink <jonathandavidbrink@gmail.com>
2025-02-24 13:07:09 -0500

    dolphins stats

--- src/data/xlsports-2024-fall.ts
+++ src/data/xlsports-2024-fall.ts
@@ -14,5 +14,126 @@ export const data: Season = {
       colors: colors.dolphins,
     },
   ],
-  weeks: [],
+  weeks: [
+    {
+      date: "Oct 19 2024",
+      teams: [
+        {
+          team: "Dolphins",
+          time: "UNK",
+          opponent: "UNK",
+          home: false,
+          score: "0-1",
+          stats: {
+            Jason: { rec: 1, td: 1, int: 0, sack: 1, safety: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Oct 26 2024",
+      teams: [
+        {
+          team: "Dolphins",
+          time: "UNK",
+          opponent: "UNK",
+          home: false,
+          score: "0-1",
+          stats: {
+            Jason: { rec: 1, td: 1, int: 0, sack: 0, safety: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Nov 2 2024",
+      teams: [
+        {
+          team: "Dolphins",
+          time: "UNK",
+          opponent: "UNK",
+          home: false,
+          score: "0-1",
+          stats: {
+            Jason: { rec: 1, td: 1, int: 0, sack: 0, safety: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Nov 9 2024",
+      teams: [
+        {
+          team: "Dolphins",
+          time: "UNK",
+          opponent: "UNK",
+          home: false,
+          score: "1-0",
+          stats: {
+            Jason: { rec: 1, td: 1, int: 0, sack: 0, safety: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Nov 16 2024",
+      teams: [
+        {
+          team: "Dolphins",
+          time: "UNK",
+          opponent: "UNK",
+          home: false,
+          score: "0-1",
+          stats: {
+            Jason: { rec: 1, td: 1, int: 0, sack: 0, safety: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Nov 30 2024",
+      teams: [
+        {
+          team: "Dolphins",
+          time: "UNK",
+          opponent: "UNK",
+          home: false,
+          score: "0-1",
+          stats: {
+            Jason: { rec: 1, td: 1, int: 0, sack: 0, safety: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Dec 7 2024",
+      teams: [
+        {
+          team: "Dolphins",
+          time: "UNK",
+          opponent: "UNK",
+          home: false,
+          score: "0-1",
+          stats: {
+            Jason: { rec: 1, td: 1, int: 0, sack: 0, safety: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Dec 14 2024",
+      teams: [
+        {
+          team: "Dolphins",
+          time: "UNK",
+          opponent: "UNK",
+          home: false,
+          score: "0-1",
+          stats: {
+            Jason: { rec: 1, td: 1, int: 0, sack: 0, safety: 0 },
+          },
+        },
+      ],
+    },
+  ],
 };

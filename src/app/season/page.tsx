cedb8e22fa3c2c8b159aa290ff5b2b5c1aaed9a8
Jonathan Brink <jonathandavidbrink@gmail.com>
2025-04-12 15:30:21 -0400

    seasons page

--- src/app/season/page.tsx
+++ src/app/season/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+import { seasons } from "@/data";
+
 export default function Page() {
-  return <div>Seasons...</div>;
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <h2>Seasons</h2>
+      <ul>
+        {seasons.map(({ id }) => (
+          <li key={id}>
+            <a href={`/season/${id}`}>{id}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }

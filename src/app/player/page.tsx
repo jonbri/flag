d5ee836c7075298b5db60992c91058cedc7c63e2
Jonathan Brink <jonathandavidbrink@gmail.com>
2025-04-12 15:38:27 -0400

    players page

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <div>this is a player</div>;
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <h2>Players</h2>
+      <ul>
+        <li>
+          <a href={`/player/Austin`}>Austin</a>
+        </li>
+        <li>
+          <a href={`/player/Jason`}>Jason</a>
+        </li>
+      </ul>
+    </div>
+  );
 }

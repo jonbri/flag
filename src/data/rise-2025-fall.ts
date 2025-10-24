8f9042a75e1d33b82972b5c5e48bf33c9af032df
Jonathan Brink <jonathandavidbrink@gmail.com>
2025-10-24 19:53:29 -0400

    Update rise-2025-fall.ts

--- src/data/rise-2025-fall.ts
+++ src/data/rise-2025-fall.ts
@@ -117,8 +117,9 @@ export const data: Season = {
           team: "Seahawks",
           opponent: "Broncos",
           time: "6:20",
+          score: "13-12",
           stats: {
-            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+            Jason: { rec: 0, td: 2, int: 1, sack: 0, safety: 0, picksix: 0 },
             Austin: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
           },
         },

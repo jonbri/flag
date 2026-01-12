0925858162142d8e1018da3dc5c4518e6bbfe993
Jonathan Brink <jonathandavidbrink@gmail.com>
2026-01-12 09:36:27 -0500

    cowboys data

--- src/data/rise-2026-winter.ts
+++ src/data/rise-2026-winter.ts
@@ -11,7 +11,16 @@ export const data: Season = {
       shortName: "DAL",
       city: "Dallas",
       brink: "Both",
-      players: ["Austin", "Jason", "Grayson", "Ben", "Paxton"],
+      players: [
+        "Austin",
+        "Jason",
+        "Grayson",
+        "Ben",
+        "Bennet",
+        "Khari",
+        "Mia",
+        "Maddox",
+      ],
       colors: colors.cowboys,
     },
   ],
@@ -21,10 +30,78 @@ export const data: Season = {
       teams: [
         {
           team: "Cowboys",
+          opponent: "Patriots",
+          time: "11:00",
+          stats: {
+            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+            Austin: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Jan 24 2026",
+      teams: [
+        {
+          team: "Cowboys",
+          opponent: "Dolphins",
+          time: "11:00",
+          stats: {
+            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+            Austin: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Jan 31 2026",
+      teams: [
+        {
+          team: "Cowboys",
+          opponent: "49ers",
+          time: "11:00",
+          stats: {
+            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+            Austin: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Feb 7 2026",
+      teams: [
+        {
+          team: "Cowboys",
+          opponent: "Lions",
+          time: "11:00",
+          stats: {
+            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+            Austin: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Feb 14 2026",
+      teams: [
+        {
+          team: "Cowboys",
+          opponent: "Bills",
+          time: "11:00",
+          stats: {
+            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+            Austin: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Jan 17 2026",
+      teams: [
+        {
+          team: "Cowboys",
           opponent: "Jaguars",
-          time: "7:10",
-          score: "1-1",
-          notes: "",
+          time: "11:00",
           stats: {
             Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
             Austin: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },

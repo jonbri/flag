660d084edfd6935fa302aec370fa36d6ce34e584
Jonathan Brink <jonathandavidbrink@gmail.com>
2026-02-14 13:51:30 -0500

    Revert "score correction"
    This reverts commit 64e6593ee651414ff5153b8c1a4572a94f44b58b.

--- src/data/rise-2026-winter.ts
+++ src/data/rise-2026-winter.ts
@@ -48,7 +48,7 @@ export const data: Season = {
           team: "Cowboys",
           opponent: "Lions",
           home: true,
-          score: "13-21",
+          score: "7-13",
           time: "11:00",
           stats: {
             Jason: { rec: 0, td: 1, int: 0, sack: 0, safety: 0, picksix: 0 },

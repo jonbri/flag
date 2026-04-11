635c8b81a17e072860ceaba9e35f2c928e34af4a
Jonathan Brink <jonathandavidbrink@gmail.com>
2026-04-11 17:06:45 -0400

    april 11

--- src/data/rduflagfootball-2026-spring.ts
+++ src/data/rduflagfootball-2026-spring.ts
@@ -49,5 +49,21 @@ export const data: Season = {
         },
       ],
     },
+    {
+      date: "Apr 11 2026",
+      teams: [
+        {
+          team: "Cobras",
+          opponent: "Pythons",
+          time: "2:00",
+          home: true,
+          score: "62-22",
+          stats: {
+            Jason: { rec: 2, td: 4, int: 1, sack: 1, safety: 0, picksix: 1 },
+            Austin: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+          },
+        },
+      ],
+    },
   ],
 };

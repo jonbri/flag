02428d0ff3fe9060b3ceeb1dee9e46b7db43cfe7
Jonathan Brink <jonathandavidbrink@gmail.com>
2026-08-24 10:58:13 -0400

    aug 22

--- src/data/rduflagfootball-2026-summer.ts
+++ src/data/rduflagfootball-2026-summer.ts
@@ -145,5 +145,21 @@ export const data: Season = {
         },
       ],
     },
+    {
+      date: "Aug 22 2026",
+      teams: [
+        {
+          team: "Green",
+          opponent: "Blue",
+          time: "1:00",
+          home: true,
+          score: "19-47",
+          stats: {
+            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+            Austin: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+          },
+        },
+      ],
+    },
   ],
 };

6f4bc8b768e355e95e4c9fbc9be7940a4cd38dc3
Jonathan Brink <jonathandavidbrink@gmail.com>
2026-09-23 10:24:49 -0400

    schedule

--- src/data/rise-2026-fall.ts
+++ src/data/rise-2026-fall.ts
@@ -38,8 +38,93 @@ export const data: Season = {
           opponent: "Cowboys",
           time: "5:30",
           score: "28-6",
+          home: false,
           stats: {
             Jason: { rec: 0, td: 2, int: 3, sack: 0, safety: 0, picksix: 1 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Sep 25 2026",
+      teams: [
+        {
+          team: "Vikings",
+          opponent: "Falcons",
+          time: "6:20",
+          home: false,
+          stats: {
+            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Oct 2 2026",
+      teams: [
+        {
+          team: "Vikings",
+          opponent: "Steelers",
+          time: "5:30",
+          home: true,
+          stats: {
+            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Oct 9 2026",
+      teams: [
+        {
+          team: "Vikings",
+          opponent: "Ravens",
+          time: "6:20",
+          home: false,
+          stats: {
+            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Oct 16 2026",
+      teams: [
+        {
+          team: "Vikings",
+          opponent: "Buccaneers",
+          time: "6:20",
+          home: false,
+          stats: {
+            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Oct 23 2026",
+      teams: [
+        {
+          team: "Vikings",
+          opponent: "Panthers",
+          time: "6:20",
+          home: true,
+          stats: {
+            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
+          },
+        },
+      ],
+    },
+    {
+      date: "Oct 30 2026",
+      teams: [
+        {
+          team: "Vikings",
+          opponent: "Bills",
+          time: "6:20",
+          home: false,
+          stats: {
+            Jason: { rec: 0, td: 0, int: 0, sack: 0, safety: 0, picksix: 0 },
           },
         },
       ],

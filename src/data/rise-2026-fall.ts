64c3294d7370fbd01cc56c42d474dfc9fbcff80f
Jonathan Brink <jonathandavidbrink@gmail.com>
2026-09-26 13:48:49 -0400

    Fix score

--- src/data/rise-2026-fall.ts
+++ src/data/rise-2026-fall.ts
@@ -53,7 +53,7 @@ export const data: Season = {
           opponent: "Falcons",
           time: "6:20",
           home: false,
-          score: "21-6",
+          score: "6-21",
           stats: {
             Jason: { rec: 0, td: 1, int: 1, sack: 0, safety: 0, picksix: 0 },
           },
